1409e1a019ab6575d864d387a3a8f796c71f6a69
lqviet45 <lqviet455@gmail.com>
2025-07-14 20:05:12 +0700

    Add BaseEntity class and update project configuration for account services

---
+++ account-services/src/main/java/com/lqviet/accountservices/entities/BaseEntity.java
@@ -0,0 +1,14 @@
+﻿package com.lqviet.accountservices.entities;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity<ID> {
+    @Id
+    private ID id;
+}
